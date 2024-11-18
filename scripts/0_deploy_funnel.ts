935ef409bf3fdeb65be8bd88eeea13fc1668b004
harshnambiar <harrisonlvl2@gmail.com>
2024-11-18 14:54:11 +0530

    added deploy script

---
+++ scripts/0_deploy_funnel.ts
@@ -0,0 +1,22 @@
+import { Deployer, DeployFunction, Network } from '@alephium/cli'
+import { Settings } from '../alephium.config'
+import { Funnel } from '../artifacts/ts'
+
+// This deploy function will be called by cli deployment tool automatically
+// Note that deployment scripts should prefixed with numbers (starting from 0)
+const deployFunnel: DeployFunction<Settings> = async (
+  deployer: Deployer
+): Promise<void> => {
+  const result = await deployer.deployContract(Funnel, {
+    
+    
+    // The initial state of the hello contract
+    initialFields: {
+      count: 0n
+    }
+  })
+  console.log('Funnel contract id: ' + result.contractInstance.contractId)
+  console.log('Funnel contract address: ' + result.contractInstance.address)
+}
+
+export default deployFunnel
---
+++ src/funnel.ts
@@ -0,0 +1,100 @@
+import { Deployments } from '@alephium/cli'
+import { DUST_AMOUNT, web3, NodeProvider, ONE_ALPH } from '@alephium/web3'
+import { PrivateKeyWallet} from '@alephium/web3-wallet'
+import configuration from '../alephium.config'
+import { Funnel } from '../artifacts/ts'
+
+async function display_count() {
+  console.log('init display')
+  //Select our network defined in alephium.config.ts
+  const network = configuration.networks.testnet
+
+  //NodeProvider is an abstraction of a connection to the Alephium network
+  // wallet-v20 is one of the only working links right now for the testnet so let's leave it at that
+  const nodeProvider = new NodeProvider('https://wallet-v20.testnet.alephium.org')
+
+  //Sometimes, it's convenient to setup a global NodeProvider for your project:
+  web3.setCurrentNodeProvider(nodeProvider)
+
+  //Connect our wallet, typically in a real application you would connect your web-extension or desktop wallet
+  const key: string = (process.env.PK as string) || "no key found";
+  const wallet = new PrivateKeyWallet({privateKey: key })
+
+  //.deployments contains the info of our `CounterRalph` deployement, as we need to now the contractId and address
+  //This was auto-generated with the `cli deploy` of our `scripts/0_deploy_hello.ts`
+  const deployments = await Deployments.load(configuration, 'testnet')
+	console.log(deployments)
+  //Make sure it match your address group
+  const accountGroup = 3
+
+  const deployed = deployments.getDeployedContractResult(accountGroup, 'Funnel')
+
+  if(deployed !== undefined) {
+    const funnelContractId = deployed.contractInstance.contractId
+    const funnelContractAddress = deployed.contractInstance.address
+    console.log(funnelContractAddress)
+    console.log("bye")
+
+    
+    // Fetch the latest state of the token contract, `mut counter` should have change
+    const funnel = Funnel.at(funnelContractAddress)
+    const state = await funnel.fetchState()
+    console.log(state.fields)
+
+   
+    console.log("au revoir")
+  } else {
+    console.log('`deployed` is undefined')
+  }
+}
+
+
+async function new_entry() {
+	console.log('init new entry');
+  //Select our network defined in alephium.config.ts
+  const network = configuration.networks.testnet
+
+  //NodeProvider is an abstraction of a connection to the Alephium network
+  const nodeProvider = new NodeProvider('https://wallet-v20.testnet.alephium.org')
+
+  //Sometimes, it's convenient to setup a global NodeProvider for your project:
+  web3.setCurrentNodeProvider(nodeProvider)
+
+  //Connect our wallet, typically in a real application you would connect your web-extension or desktop wallet
+  const key: string = (process.env.PK as string) || "no key found";
+  const wallet = new PrivateKeyWallet({privateKey: key, nodeProvider})
+
+  //.deployments contains the info of our `TokenFaucet` deployement, as we need to now the contractId and address
+  //This was auto-generated with the `cli deploy` of our `scripts/0_deploy_faucet.ts`
+  const deployments = await Deployments.load(configuration, 'testnet')
+	
+  //Make sure it match your address group
+  const accountGroup = 3
+
+  const deployed = deployments.getDeployedContractResult(accountGroup, 'Funnel')
+
+  if(deployed !== undefined) {
+    const funnelContractId = deployed.contractInstance.contractId
+    const funnelContractAddress = deployed.contractInstance.address
+
+    const funnel = Funnel.at(funnelContractAddress)
+    await funnel.transact.createEntry({
+    	signer: wallet,
+   	args: { amt: 1n, to: '12sxfxraVoU8FcSVd7P2SVr2cd2vi8d17KtrprrL7cBbV', ag: '12sxfxraVoU8FcSVd7P2SVr2cd2vi8d17KtrprrL7cBbV'},
+   	attoAlphAmount: ONE_ALPH + DUST_AMOUNT
+   })
+	//100000000000000000
+	//1000000000000000
+   
+    console.log("au revoir 2")
+  } else {
+    console.log('`deployed` is undefined')
+  }
+}
+
+
+
+// Let's perform one operation at a time while commenting the others out
+//display_count()
+new_entry()
+
