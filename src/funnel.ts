ff47f01b11b5acca4dac76397c20b93a8f82273e
harshnambiar <harrisonlvl2@gmail.com>
2024-12-12 13:59:55 +0530

    more functions added to node test run

--- src/funnel.ts
+++ src/funnel.ts
@@ -5,7 +5,7 @@ import configuration from '../alephium.config'
 import { Funnel } from '../artifacts/ts'
 
 async function display_count() {
-  console.log('init display')
+  console.log('init display count')
   //Select our network defined in alephium.config.ts
   const network = configuration.networks.testnet
 
@@ -48,6 +48,50 @@ async function display_count() {
   }
 }
 
+
+async function display_leading_amount() {
+  console.log('init display lead amount')
+  //Select our network defined in alephium.config.ts
+  const network = configuration.networks.testnet
+
+  //NodeProvider is an abstraction of a connection to the Alephium network
+  // wallet-v20 is one of the only working links right now for the testnet so let's leave it at that
+  const nodeProvider = new NodeProvider('https://wallet-v20.testnet.alephium.org')
+
+  //Sometimes, it's convenient to setup a global NodeProvider for your project:
+  web3.setCurrentNodeProvider(nodeProvider)
+
+  //Connect our wallet, typically in a real application you would connect your web-extension or desktop wallet
+  const key: string = (process.env.PK as string) || "no key found";
+  const wallet = new PrivateKeyWallet({privateKey: key })
+
+  //.deployments contains the info of our `CounterRalph` deployement, as we need to now the contractId and address
+  //This was auto-generated with the `cli deploy` of our `scripts/0_deploy_hello.ts`
+  const deployments = await Deployments.load(configuration, 'testnet')
+	console.log(deployments)
+  //Make sure it match your address group
+  const accountGroup = 3
+
+  const deployed = deployments.getDeployedContractResult(accountGroup, 'Funnel')
+
+  if(deployed !== undefined) {
+    const funnelContractId = deployed.contractInstance.contractId
+    const funnelContractAddress = deployed.contractInstance.address
+    console.log(funnelContractAddress)
+    console.log("bye")
+
+    
+    // Fetch the latest state of the token contract, `mut counter` should have change
+    const funnel = Funnel.at(funnelContractAddress)
+    const state = await funnel.fetchState()
+    console.log(state.fields.leadingamount)
+
+   
+    console.log("au revoir 3")
+  } else {
+    console.log('`deployed` is undefined')
+  }
+}
 
 async function new_entry() {
 	console.log('init new entry');
@@ -95,6 +139,6 @@ async function new_entry() {
 
 
 // Let's perform one operation at a time while commenting the others out
-display_count()
+//display_count()
 //new_entry()
-
+display_leading_amount()
